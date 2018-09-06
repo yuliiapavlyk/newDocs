225cf0a95ed9b9b6a6d95f240563ca83857d3f87
Yuliia Pavlyk <ypavly@softserveinc.com>
2018-09-06 17:37:50 +0300

    added end-point

---
+++ back end/MyDocuments/MyDocuments.PL/Controllers/DocumentController.cs
@@ -0,0 +1,106 @@
+﻿using MyDocuments.BLL.DTO;
+using MyDocuments.BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyDocuments.PL.Controllers
+{
+    public class DocumentController : ApiController
+    {
+
+
+        /// <summary>
+        /// Return all documents in our system
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/allDocuments")]
+        public HttpResponseMessage Get()
+        {
+            var documents = new List<DocumentDTO>();
+            documents.Add(new DocumentDTO { Name = "CV1", Description = "my own cv", Author = "Andrii Lysyi", Create_Date = DateTime.UtcNow });
+            documents.Add(new DocumentDTO { Name = "CV2", Description = "my own cv", Author = "Andrii Lysyi", Create_Date = DateTime.UtcNow });
+            documents.Add(new DocumentDTO { Name = "CV3", Description = "my own cv", Author = "Andrii Lysyi", Create_Date = DateTime.UtcNow });
+            documents.Add(new DocumentDTO { Name = "CV4", Description = "my own cv", Author = "Andrii Lysyi", Create_Date = DateTime.UtcNow });
+
+
+            if (documents.Count != 0)
+            {
+                return Request.CreateResponse<IEnumerable<DocumentDTO>>(HttpStatusCode.OK, documents);
+            }
+            const string message = "No documents in database.";
+            return Request.CreateErrorResponse(HttpStatusCode.NoContent, message);
+        }
+        
+
+        
+        /// <summary>
+        /// Return specific document by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/document/{id}")]
+
+        public HttpResponseMessage Get(int id)
+        {
+            //var document = DocumentService.GetDocumentById(id);
+            //if (document!=null)
+            //{
+            //    return Request.CreateResponse(HttpStatusCode.OK, document);
+            //}
+
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"There isn't document with id = {id}");
+        }
+
+       
+        /// <summary>
+        /// Create new document
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("api/newDocument")]
+        public HttpResponseMessage Post([FromBody]string value)
+        {
+
+            
+            var documents = new List<DocumentDTO>();
+            documents.Add(new DocumentDTO { Name = "CV1", Description = "my own cv", Author = "Andrii Lysyi", Create_Date = DateTime.UtcNow });
+            return Request.CreateResponse<IEnumerable<DocumentDTO>>(HttpStatusCode.OK, documents);
+        }
+
+        /// <summary>
+        /// Update specific document by id
+        /// </summary>
+        /// <param name="id">Id of document where we need to set new data</param>
+        /// <param name="value"> Data which we need to change</param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("api/updateDocument/{id}/update/{value}")]
+        public HttpResponseMessage Put(int id, [FromBody]string value)
+        {
+            var documents = new List<DocumentDTO>();
+            return Request.CreateResponse<IEnumerable<DocumentDTO>>(HttpStatusCode.OK, documents);
+
+        }
+
+
+        /// <summary>
+        /// Delete specific document by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("api/deleteDocument/{id}")]
+        public HttpResponseMessage Delete(int id)
+        {
+            var documents = new List<DocumentDTO>();
+            return Request.CreateResponse<IEnumerable<DocumentDTO>>(HttpStatusCode.OK, documents);
+        }
+    }
+}
