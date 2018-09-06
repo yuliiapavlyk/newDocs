ffc21ff8647457d2b3bf129a7bd695fb95361755
ALysyi <andrii.lysyi1@gmail.com>
2018-09-06 10:38:54 +0300

    resolved conflicts

---
+++ back end/MyDocuments/MyDocuments.BLL/Services/DocumentService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyDocuments.BLL.Interfaces;
+using MyDocuments.DAL.Repositories.Interfaces;
+using MyDocuments.BLL.DTO;
+using MyDocuments.DAL.Entities;
+
+namespace MyDocuments.BLL.Services
+{
+    public class DocumentService: BaseService, IDocumentService
+    {
+        public DocumentService(IUnitOfWork db) : base(db)
+        {
+        }
+        public IEnumerable<DocumentDTO> GetAllDocuments()
+        {
+            var documents = db.Documents.GetAll();
+            var documentsDTO = new List<DocumentDTO>();
+            if(documents == null)
+            {
+                return null;
+            }
+            foreach(var doc in documents)
+            {
+                documentsDTO.Add(DocumentToDocumentDTO(doc));
+            }
+            return documentsDTO;
+        }
+        public DocumentDTO GetDocumentById(int id)
+        {
+            var document = db.Documents.Get(id);
+            if (document == null) return null;
+            return DocumentToDocumentDTO(document);
+        }
+        public bool RemoveDocumentById(int id)
+        {
+            var document = db.Documents.Get(id);
+            if (document != null)
+            {
+                db.Documents.Remove(id);
+                db.Save();
+                return true;
+            }
+            return false;
+        }
+        public bool UpdateDocumentById(int id, DocumentDTO dto)
+        {
+            var document = db.Documents.Get(id);
+            if (document == null) return false;
+            if (!string.IsNullOrEmpty(dto.Name))
+            {
+                document.Name = dto.Name;
+            }
+            if (!string.IsNullOrEmpty(dto.Description))
+            {
+                document.Description = dto.Description;
+            }
+            if (dto.Modified_Date != null)
+            {
+                document.Modified_Date = dto.Modified_Date;
+            }
+            db.Documents.Update(document);
+            db.Save();
+            return true;
+        }
+        private DocumentDTO DocumentToDocumentDTO(Document document)
+        {
+            return new DocumentDTO(document.Id,
+                                document.Name,
+                                document.Description,
+                                document.Author,
+                                document.Create_Date,
+                                document.Modified_Date!=null? document.Modified_Date :null
+                                );
+        }
+    }
+}
---
+++ back end/MyDocuments/MyDocuments.DAL/Repositories/DocumentRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MyDocuments.DAL.Repositories.Interfaces;
+using MyDocuments.DAL.Entities;
+using MyDocuments.DAL.EF;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace MyDocuments.DAL.Repositories
+{
+    public class DocumentRepository : IRepository<Document>
+    {
+        private DocumentContext db;
+
+        public DocumentRepository(DocumentContext context)
+        {
+            this.db = context;
+        }
+        public IEnumerable<Document> GetAll()
+        {
+            return db.Documents;
+        }
+        public Document Get(int id)
+        {
+            return db.Documents.Find(id);
+        }
+        public void Add (Document Doc)
+        {
+            db.Documents.Add(Doc);
+        }
+        public void Update(Document Doc)
+        {
+            db.Entry(Doc).State =EntityState.Modified;
+        }
+        public void Remove(int id)
+        {
+            Document Doc = db.Documents.Find(id);
+            if (Doc != null)
+                db.Documents.Remove(Doc);
+        }
+    }
+}
